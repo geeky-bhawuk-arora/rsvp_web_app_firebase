4e5f14693b8e400518c7fe47302319f8196519d5
geeky-bhawuk-arora <112162852+geeky-bhawuk-arora@users.noreply.github.com>
2023-03-09 13:25:25 +0530

    1 file modified, 1 deleted

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 // Import stylesheets
 import './style.css';
+
 // Firebase App (the core Firebase SDK) is always required
 import { initializeApp } from 'firebase/app';
 
 // Add the Firebase products and methods that you want to use
-import {  getAuth, EmailAuthProvider, signOut, onAuthStateChanged } from 'firebase/auth';
+import {
+  getAuth,
+  EmailAuthProvider,
+  signOut,
+  onAuthStateChanged,
+} from 'firebase/auth';
+
 import {} from 'firebase/firestore';
 
 import * as firebaseui from 'firebaseui';
@@ -28,33 +35,34 @@ let db, auth;
 async function main() {
   // Add Firebase project configuration object here
   const firebaseConfig = {
-    apiKey: "AIzaSyCCP9Xx4n_XY7nYVhaIpQWyNUlb218v5AI",
-    authDomain: "my-first-app-f0c9d.firebaseapp.com",
-    projectId: "my-first-app-f0c9d",
-    storageBucket: "my-first-app-f0c9d.appspot.com",
-    messagingSenderId: "811132480307",
-    appId: "1:811132480307:web:9c78c0e0566704f701ec46"
+    apiKey: 'AIzaSyCCP9Xx4n_XY7nYVhaIpQWyNUlb218v5AI',
+    authDomain: 'my-first-app-f0c9d.firebaseapp.com',
+    projectId: 'my-first-app-f0c9d',
+    storageBucket: 'my-first-app-f0c9d.appspot.com',
+    messagingSenderId: '811132480307',
+    appId: '1:811132480307:web:9c78c0e0566704f701ec46',
   };
-  // Listen to RSVP button clicks
-  startRsvpButton.addEventListener('click', () => {
-    if (auth.currentUser) {
-      // User is signed in; allows user to sign out
-      signOut(auth);
-    } else {
-      // No user is signed in; allows user to sign in
-      ui.start('#firebaseui-auth-container', uiConfig);
-    }
-  });
 
-   // Listen to the current Auth state
-   onAuthStateChanged(auth, user => {
-    if (user) {
-      startRsvpButton.textContent = 'LOGOUT';
-    } else {
-      startRsvpButton.textContent = 'RSVP';
-    }
-  });
-  
+ // Called when the user clicks the RSVP button
+startRsvpButton.addEventListener('click', () => {
+  if (auth.currentUser) {
+    // User is signed in; allows user to sign out
+    signOut(auth);
+  } else {
+    // No user is signed in; allows user to sign in
+    ui.start('#firebaseui-auth-container', uiConfig);
+  }
+});
+
+// Listen to the current Auth state
+onAuthStateChanged(auth, user => {
+  if (user) {
+    startRsvpButton.textContent = 'LOGOUT';
+  } else {
+    startRsvpButton.textContent = 'RSVP';
+  }
+});
+
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
   auth = getAuth();
@@ -77,6 +85,6 @@ async function main() {
 
   // Initialize the FirebaseUI widget using Firebase
   const ui = new firebaseui.auth.AuthUI(auth);
-};
+}
 
 main();
